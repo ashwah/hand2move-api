a585333b5acb7e21f00864b24fab33900b8c8a4a
Ashley George <ashwah@gmail.com>
2023-01-11 20:06:23 +0000

    Enabling a timestamp GQL object type.

--- database.js
+++ database.js
@@ -134,51 +134,51 @@ What.belongsTo(Job);
 Chat.hasMany(Message);
 Message.belongsTo(Chat);
 
-Conn.sync({ force: true }).then(()=> {
-  _.times(10, () => {
-    return Job.create({
-      user_id: faker.datatype.number(),
-    }).then(job => {
-      let n = faker.datatype.number({ min: 1, max: 2 });
-      _.times(n, () => {
-        return job.createWhat({
-          job_id: job.id,
-          length: faker.datatype.number(),
-          status: faker.color.human(),
-          width: faker.datatype.number(),
-          height: faker.datatype.number(),
-          weight: faker.datatype.number(),
-          is_fragile: faker.datatype.boolean(),
-          is_temperature_sensitive: faker.datatype.boolean(),
-        }); 
-      })
-    });
-  });
+// Conn.sync({ force: true }).then(()=> {
+//   _.times(10, () => {
+//     return Job.create({
+//       user_id: faker.datatype.number(),
+//     }).then(job => {
+//       let n = faker.datatype.number({ min: 1, max: 2 });
+//       _.times(n, () => {
+//         return job.createWhat({
+//           job_id: job.id,
+//           length: faker.datatype.number(),
+//           status: faker.color.human(),
+//           width: faker.datatype.number(),
+//           height: faker.datatype.number(),
+//           weight: faker.datatype.number(),
+//           is_fragile: faker.datatype.boolean(),
+//           is_temperature_sensitive: faker.datatype.boolean(),
+//         }); 
+//       })
+//     });
+//   });
 
-  _.times(6, () => {
-    return Chat.create({
-      user_chat_id: faker.datatype.number({ min: 1, max: 100 }),
-    }).then(chat => {
-      return User.create({
-        name: faker.name.fullName(),
-      }).then(user1 => {
-        return User.create({
-          name: faker.name.fullName(),
-        }).then(user2 => {
-          let m = faker.datatype.number({ min:50, max: 100 })
-          _.times(m, () => {
-            Message.create({
-              chat_id: chat.id,
-              chatId: chat.id,
-              user_id: Math.random() < 0.5 ? user1.id : user2.id,
-              message: faker.science.chemicalElement().name,
-              date: faker.datatype.number({ min: 1641759318000, max: 1673295343000 }),
-            });
-          });
-        });
-      });
-    });
-  });
-});
+//   _.times(6, () => {
+//     return Chat.create({
+//       user_chat_id: faker.datatype.number({ min: 1, max: 100 }),
+//     }).then(chat => {
+//       return User.create({
+//         name: faker.name.fullName(),
+//       }).then(user1 => {
+//         return User.create({
+//           name: faker.name.fullName(),
+//         }).then(user2 => {
+//           let m = faker.datatype.number({ min:50, max: 100 })
+//           _.times(m, () => {
+//             Message.create({
+//               chat_id: chat.id,
+//               chatId: chat.id,
+//               user_id: Math.random() < 0.5 ? user1.id : user2.id,
+//               message: faker.science.chemicalElement().name,
+//               date: faker.datatype.number({ min: 1641759318000, max: 1673295343000 }),
+//             });
+//           });
+//         });
+//       });
+//     });
+//   });
+// });
 
 module.exports = Conn;
--- schema.js
+++ schema.js
@@ -6,7 +6,32 @@ const {
   GraphQLSchema,
   GraphQLList,
   GraphQLNonNull,
+  GraphQLScalarType 
 } = require('graphql');
+
+const Kind = require('graphql/language')
+
+const TimestampType = new GraphQLScalarType({
+  name: 'Timestamp',
+  serialize(date) {
+    return (date instanceof Date) ? date.getTime() : null
+  },
+  parseValue(date) {
+    try           { return new Date(value); }
+    catch (error) { return null; }
+  },
+  parseLiteral(ast) {
+    if (ast.kind === Kind.INT) {
+      return new Date(parseInt(ast.value, 10));
+    }
+    else if (ast.kind === Kind.STRING) {
+      return this.parseValue(ast.value);
+    }
+    else {
+      return null;
+    }
+  },
+});
 
 const Db = require('./database');
 
@@ -139,7 +164,6 @@ const Chat = new GraphQLObjectType({
   }
 });
 
-
 const Message = new GraphQLObjectType({
   name: 'Message',
   description: 'An individual message',
@@ -164,7 +188,7 @@ const Message = new GraphQLObjectType({
         }
       },
       date: {
-        type: GraphQLString,
+        type: TimestampType,
         resolve (message) {
           return message.date;
         }
@@ -269,7 +293,7 @@ const Query = new GraphQLObjectType({
             type: GraphQLInt
           },
           date: {
-            type: GraphQLString
+            type: TimestampType
           },
           message: {
             type: GraphQLString
