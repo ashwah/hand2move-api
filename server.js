3ed6c8aa6726b52ba332b6932dc7a9de1b5f2c98
Ashley George <ashwah@gmail.com>
2023-01-14 17:15:55 +0000

    Allowing CORS.

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const Schema = require('./schema'); 
+const cors = require('cors');
 
 const app = express();
- 
+
+// Allow CORS.
+app.use(cors());
+
 app.use(
   '/graphql',
   graphqlHTTP({
